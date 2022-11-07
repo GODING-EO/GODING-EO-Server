07d0a6a25484f8b0f4f2532c8df3ccf6738e619c
Sangjin <sangjin170202@naver.com>
2022-11-07 09:19:18 +0900

    chore(#7): GodingeoException 추가

---
+++ src/main/java/kr/co/godingeo/global/error/exception/GodingeoException.java
@@ -0,0 +1,11 @@
+package kr.co.godingeo.global.error.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class GodingeoException extends RuntimeException {
+
+    private final GlobalErrorCode globalErrorCode;
+}
