b4833ff82810c98278019066a8ffc1957f562955
Sangjin <sangjin170202@naver.com>
2022-11-07 09:10:32 +0900

    feat(#3): BaseEntity 추가

---
+++ src/main/java/kr/co/godingeo/global/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package kr.co.godingeo.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private final ZonedDateTime createdAt = ZonedDateTime.now();
+
+    @LastModifiedDate
+    private final ZonedDateTime modifiedAt = ZonedDateTime.now();
+
+}
