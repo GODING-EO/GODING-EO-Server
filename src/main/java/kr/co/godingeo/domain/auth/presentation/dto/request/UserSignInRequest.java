ec9e6b7ea5fc8cbab31979dec1640970dc70669e
Sangjin <sangjin170202@naver.com>
2022-11-16 08:35:36 +0900

    feat(#13): UserSignInRequest 추가

---
+++ src/main/java/kr/co/godingeo/domain/auth/presentation/dto/request/UserSignInRequest.java
@@ -0,0 +1,18 @@
+package kr.co.godingeo.domain.auth.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class UserSignInRequest {
+
+    @NotBlank(message = "account_id는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    private String accountId;
+
+    @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    private String password;
+
+}
