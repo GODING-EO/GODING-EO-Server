a2b8a40c7164119a548139b92a3c3f5e3d85bc08
Sangjin <sangjin170202@naver.com>
2022-11-16 11:57:21 +0900

    refactor(#15): BaseEntity 수정

--- src/main/java/kr/co/godingeo/global/entity/BaseEntity.java
+++ src/main/java/kr/co/godingeo/global/entity/BaseEntity.java
@@ -5,19 +5,22 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.ZonedDateTime;
+import java.time.LocalDate;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
+    @Column(nullable = false)
     @CreatedDate
-    private final ZonedDateTime createdAt = ZonedDateTime.now();
+    private LocalDate createdDate;
 
+    @Column(nullable = false)
     @LastModifiedDate
-    private final ZonedDateTime modifiedAt = ZonedDateTime.now();
+    private LocalDate modifiedDate;
 
 }
