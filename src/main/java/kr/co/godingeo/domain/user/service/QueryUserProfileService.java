022730e14c1426a08517ab3193e40dfb1da28651
Sangjin <sangjin170202@naver.com>
2022-11-20 08:46:29 +0900

    refactor(#37): QueryUserProfileService 변경

--- src/main/java/kr/co/godingeo/domain/user/service/QueryUserProfileService.java
+++ src/main/java/kr/co/godingeo/domain/user/service/QueryUserProfileService.java
@@ -2,6 +2,7 @@ package kr.co.godingeo.domain.user.service;
 
 import kr.co.godingeo.domain.user.domain.User;
 import kr.co.godingeo.domain.user.presentation.dto.response.QueryUserInfoResponse;
+import kr.co.godingeo.domain.user.presentation.dto.response.QueryUserProfileResponse;
 import kr.co.godingeo.domain.user.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,10 @@ public class QueryUserProfileService {
 
     private final UserUtil userUtil;
 
-    public QueryUserInfoResponse execute(Long userId) {
+    public QueryUserProfileResponse execute(Long userId) {
         User user = userUtil.getUserById(userId);
 
-        return QueryUserInfoResponse.builder()
+        return QueryUserProfileResponse.builder()
                 .name(user.getName())
                 .job(user.getJob())
                 .build();
