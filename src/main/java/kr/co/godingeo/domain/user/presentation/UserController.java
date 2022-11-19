c344ae1f55ef13c9ad51b5d524ce030c71e26cd7
Sangjin <sangjin170202@naver.com>
2022-11-20 08:43:52 +0900

    feat(#37): API Swagger 적용

--- src/main/java/kr/co/godingeo/domain/user/presentation/UserController.java
+++ src/main/java/kr/co/godingeo/domain/user/presentation/UserController.java
@@ -1,10 +1,12 @@
 package kr.co.godingeo.domain.user.presentation;
 
+import io.swagger.annotations.ApiOperation;
 import kr.co.godingeo.domain.auth.presentation.dto.response.UserTokenResponse;
 import kr.co.godingeo.domain.user.presentation.dto.request.UpdatePasswordRequest;
 import kr.co.godingeo.domain.user.presentation.dto.request.UpdateUserInfoRequest;
 import kr.co.godingeo.domain.user.presentation.dto.request.UserSignUpRequest;
 import kr.co.godingeo.domain.user.presentation.dto.response.QueryUserInfoResponse;
+import kr.co.godingeo.domain.user.presentation.dto.response.QueryUserProfileResponse;
 import kr.co.godingeo.domain.user.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,45 +28,61 @@ public class UserController {
     private final UpdateUserInfoService updateUserInfoService;
     private final UpdatePasswordService updatePasswordService;
 
+
+    @ApiOperation(value = "회원가입")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public UserTokenResponse userSignUp(@RequestBody @Valid UserSignUpRequest request) {
         return userSignUpService.execute(request);
     }
 
+
+    @ApiOperation(value = "로그아웃")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/logout")
     public void logout() {
         userLogoutService.execute();
     }
 
+
+    @ApiOperation(value = "회원탈퇴")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/leave")
     public void leave() {
         userWithdrawalService.execute();
     }
 
+
+    @ApiOperation(value = "마이 페이지 보기")
     @GetMapping
     public QueryUserInfoResponse queryMyPage() {
         return queryMypageService.execute();
     }
 
+
+    @ApiOperation(value = "다른 유저 정보 가져오기")
     @GetMapping("/{user-id}")
-    public QueryUserInfoResponse queryUserProfile(@PathVariable("user-id") Long userId) {
+    public QueryUserProfileResponse queryUserProfile(@PathVariable("user-id") Long userId) {
         return queryUserProfileService.execute(userId);
     }
 
+
+    @ApiOperation(value = "내 정보 수정 페이지 방문하기")
     @GetMapping("/info")
     public QueryUserInfoResponse queryMyInfo() {
         return queryMyInfoService.execute();
     }
 
+
+    @ApiOperation(value = "내 정보 수정하기")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping
     public void updateUserInfo(@RequestBody @Valid UpdateUserInfoRequest request) {
         updateUserInfoService.execute(request);
     }
 
+
+    @ApiOperation(value = "비밀번호 바꾸기")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/password")
     public void updatePassword(@RequestBody @Valid UpdatePasswordRequest request) {
